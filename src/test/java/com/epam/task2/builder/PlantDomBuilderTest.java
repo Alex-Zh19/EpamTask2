28d804d73bf13292546debde778c741ff9dba65c
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-04-18 16:27:23 +0300

    main deleted,tests updated

--- src/test/java/com/epam/task2/builder/PlantDomBuilderTest.java
+++ src/test/java/com/epam/task2/builder/PlantDomBuilderTest.java
@@ -1,12 +1,16 @@
 package com.epam.task2.builder;
 
+import com.epam.task2.entity.GrowingTips;
 import com.epam.task2.entity.PlantEntity;
 import com.epam.task2.entity.PlantOrigin;
+import com.epam.task2.entity.VisualParameter;
 import com.epam.task2.exception.PlantException;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,6 +26,28 @@ public class PlantDomBuilderTest {
         URL pathToFileUrl = classLoader.getResource(pth);
         plantDomBuilder.buildPlant(new File(pathToFileUrl.getFile()).getAbsolutePath());
         Set<PlantEntity> setActual= plantDomBuilder.getPlants();
-        assertEquals(setActual.size(),16);
+
+        PlantEntity oneOfThePlants=new PlantEntity();
+        oneOfThePlants.setId("hcnth1");
+        oneOfThePlants.setOrigin(PlantOrigin.USA);
+        oneOfThePlants.setName("Hyacinth");
+        oneOfThePlants.setSoil("podzolic");
+        LocalDateTime dateTime=LocalDateTime.parse("2011-07-01T00:00:02");
+        oneOfThePlants.setPlantingTime(dateTime);
+        VisualParameter visualParameter=new VisualParameter();
+        visualParameter.setStemColor("green");
+        visualParameter.setLeavesColor("green");
+        visualParameter.setAverageSizeOfPlant("average");
+        oneOfThePlants.setVisualParameter(visualParameter);
+        GrowingTips growingTips=new GrowingTips();
+        growingTips.setTemperature(12);
+        growingTips.setLightning("no");
+        growingTips.setWatering(45);
+        oneOfThePlants.setGrowingTips(growingTips);
+        oneOfThePlants.setMultiplying("cuttings");
+        assertTrue(setActual.contains(oneOfThePlants));
+
     }
+
+
 }
--- src/test/java/com/epam/task2/builder/PlantSaxBuilderTest.java
+++ src/test/java/com/epam/task2/builder/PlantSaxBuilderTest.java
@@ -1,11 +1,15 @@
 package com.epam.task2.builder;
 
+import com.epam.task2.entity.GrowingTips;
 import com.epam.task2.entity.PlantEntity;
+import com.epam.task2.entity.PlantOrigin;
+import com.epam.task2.entity.VisualParameter;
 import com.epam.task2.exception.PlantException;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.Set;
 
 import static org.testng.Assert.*;
@@ -20,7 +24,25 @@ public class PlantSaxBuilderTest {
         URL pathToFileUrl = classLoader.getResource(pth);
         plantSaxBuilder.buildPlant(new File(pathToFileUrl.getFile()).getAbsolutePath());
         Set<PlantEntity> setActual= plantSaxBuilder.getPlants();
-        assertEquals(setActual.size(),16);
 
+        PlantEntity oneOfThePlants=new PlantEntity();
+        oneOfThePlants.setId("hcnth1");
+        oneOfThePlants.setOrigin(PlantOrigin.USA);
+        oneOfThePlants.setName("Hyacinth");
+        oneOfThePlants.setSoil("podzolic");
+        LocalDateTime dateTime=LocalDateTime.parse("2011-07-01T00:00:02");
+        oneOfThePlants.setPlantingTime(dateTime);
+        VisualParameter visualParameter=new VisualParameter();
+        visualParameter.setStemColor("green");
+        visualParameter.setLeavesColor("green");
+        visualParameter.setAverageSizeOfPlant("average");
+        oneOfThePlants.setVisualParameter(visualParameter);
+        GrowingTips growingTips=new GrowingTips();
+        growingTips.setTemperature(12);
+        growingTips.setLightning("no");
+        growingTips.setWatering(45);
+        oneOfThePlants.setGrowingTips(growingTips);
+        oneOfThePlants.setMultiplying("cuttings");
+        assertTrue(setActual.contains(oneOfThePlants));
     }
 }
