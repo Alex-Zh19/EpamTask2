49b3dd03ec3ababc5ba099647579a6c89bf8435c
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-04-19 18:29:20 +0300

    v1.0

--- src/test/java/com/epam/task2/builder/PlantSaxBuilderTest.java
+++ src/test/java/com/epam/task2/builder/PlantSaxBuilderTest.java
@@ -43,6 +43,7 @@ public class PlantSaxBuilderTest {
         growingTips.setWatering(45);
         oneOfThePlants.setGrowingTips(growingTips);
         oneOfThePlants.setMultiplying("cuttings");
+
         assertTrue(setActual.contains(oneOfThePlants));
     }
 }
